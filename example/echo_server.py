80d1d5d0dc34f4fbc9cb55160a285a0b050450ed
Chaobin Tang <ctang@redhat.com>
2012-10-24 13:10:29 -0700

    REV: Added utils and conf module.

--- example/echo_server.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# Author: <Chaobin Tang ctang@redhat.com>
-from frog.contrib.events.handlers.base import BaseJsonEventHandler
-
-class EchoEventHandler(BaseJsonEventHandler):
-
-    def proceed(self):
-        return {'type': 'reply', 'data': {}}
